d032fcbbb44fe8611dc247714291204b53228474
Vladick Limonadick <149509967+VladickLimonadick@users.noreply.github.com>
2023-12-13 19:31:15 +0300

    Update printNaturalNumbersRecursive.h

--- hw8/hw8_4/printNaturalNumbersRecursive.h
+++ hw8/hw8_4/printNaturalNumbersRecursive.h
@@ -1,8 +1,4 @@
 #pragma once
-#ifndef PRINT_NATURAL_NUMBERS_RECURSIVE_H
-#define PRINT_NATURAL_NUMBERS_RECURSIVE_H
 
 void printNaturalNumbersDescendingRecursive(int count, int current, bool isFirstCall = true);
 void printNaturalNumbersAscendingRecursive(int count, int current = 1, bool isFirstCall = true);
-
-#endif // PRINT_NATURAL_NUMBERS_RECURSIVE_H
