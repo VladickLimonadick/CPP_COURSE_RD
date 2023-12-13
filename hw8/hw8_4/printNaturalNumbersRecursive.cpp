cd32eeb53dc48cf9ae720ae9639608ac70a0939e
Vladick Limonadick <149509967+VladickLimonadick@users.noreply.github.com>
2023-12-13 19:37:15 +0300

    Update printNaturalNumbersRecursive.cpp
    update 0,1

--- hw8/hw8_4/printNaturalNumbersRecursive.cpp
+++ hw8/hw8_4/printNaturalNumbersRecursive.cpp
@@ -1,30 +1,16 @@
 #include "printNaturalNumbersRecursive.h"
 #include <iostream>
 
-void printNaturalNumbersDescendingRecursive(int count, int current, bool isFirstCall) {
+void printNaturalNumbersDescendingRecursive(int count, int current) {
     if (count <= 0) {
         std::cout << std::endl;
         return;
     }
 
-    if (isFirstCall) {
+    if (count == current) {
         std::cout << "The natural numbers in descending order are: ";
     }
 
     std::cout << current << " ";
-    printNaturalNumbersDescendingRecursive(count - 1, current - 1, false);
-}
-
-void printNaturalNumbersAscendingRecursive(int count, int current, bool isFirstCall) {
-    if (current > count) {
-        std::cout << std::endl;
-        return;
-    }
-
-    if (isFirstCall) {
-        std::cout << "The natural numbers in ascending order are: ";
-    }
-
-    std::cout << current << " ";
-    printNaturalNumbersAscendingRecursive(count, current + 1, false);
+    printNaturalNumbersDescendingRecursive(count - 1, current - 1);
 }
